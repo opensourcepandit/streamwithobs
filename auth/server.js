cb749f1a4a520160da360a7200450ec347f79fe1
Joy <opensourcepundit@gmail.com>
2023-08-30 01:10:43 +0530

    test url form /hls to /data

--- auth/server.js
+++ auth/server.js
@@ -1,40 +1,40 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const jwt = require('jwtwebtoken');
+// const dotenv = require('dotenv');
+// const jwt = require('jwtwebtoken');
 
 const app = express();
-dotenv.config();
+// dotenv.config();
 
 // JWT token to authenticate 
-app.post("/auth/token/generateToken", function(res, req) {
-    let jwtSecretKey = process.env.JWT_SECRET_KEY;
-    let data = {
-        userId: 5426,
-        time: Date()
-    }
-    const token = jwt.sign(data, jwtSecretKey);
-    res.send(token);
-});
+// app.post("/auth/token/generateToken", function(res, req) {
+//     let jwtSecretKey = process.env.JWT_SECRET_KEY;
+//     let data = {
+//         userId: 5426,
+//         time: Date()
+//     }
+//     const token = jwt.sign(data, jwtSecretKey);
+//     res.send(token);
+// });
 
 
-app.get("/auth/token/validateToken", (req, res) => {
+// app.get("/auth/token/validateToken", (req, res) => {
   
-    let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
-    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+//     let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+//     let jwtSecretKey = process.env.JWT_SECRET_KEY;
   
-    try {
-        const token = req.header(tokenHeaderKey);
+//     try {
+//         const token = req.header(tokenHeaderKey);
   
-        const verified = jwt.verify(token, jwtSecretKey);
-        if(verified){
-            return res.send("Successfully Verified");
-        }else{
-            return res.status(401).send(error);
-        }
-    } catch (error) {
-        return res.status(401).send(error);
-    }
-});
+//         const verified = jwt.verify(token, jwtSecretKey);
+//         if(verified){
+//             return res.send("Successfully Verified");
+//         }else{
+//             return res.status(401).send(error);
+//         }
+//     } catch (error) {
+//         return res.status(401).send(error);
+//     }
+// });
 
 
 // OBS
